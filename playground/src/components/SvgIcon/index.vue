02c342737bce892c91fd108e425d3f8e2dfc0de9
liwb <lwbhtml@163.com>
2025-08-29 14:52:26 +0800

    feat:添加 SvgIcon 组件及相关图标，包含图标导入逻辑和示例 SVG 文件。

---
+++ playground/src/components/SvgIcon/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <svg :class="['svg-icon', className]" aria-hidden="true" @click="clickHandle">
+    <use :xlink:href="'#icon-' + (iconName || name)" />
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'SvgIcon',
+    props: {
+      iconName: {
+        type: String,
+        required: false
+      },
+      name: {
+        type: String,
+        required: false
+      },
+      className: {
+        type: [String, Array],
+        default: ''
+      }
+    },
+    mounted() {
+      if (!this.name && !this.iconName) {
+        console.error('Missing required prop: "iconName or name"');
+      }
+    },
+    methods: {
+      clickHandle() {
+        this.$emit('click');
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .svg-icon {
+    width: 1em;
+    height: 1em;
+    fill: currentColor;
+    vertical-align: -0.15em;
+    overflow: visible;
+    position: relative;
+  }
+</style>
---
+++ playground/src/icons/index.js
@@ -0,0 +1,12 @@
+const req = require.context('./svg', false, /\.svg$/);
+const requireAll = (requireContext) => requireContext.keys();
+
+// 提供页面使用
+requireAll(req).map(req);
+
+const re = /\.\/(.*)\.svg/;
+
+requireAll(req).map((i) => {
+  return i.match(re)[1];
+});
+
